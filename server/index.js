3bd4f92f8afaf6ef449d98e404b517d77b75eb36
VasquezMichael <vasquez.michael.steven@gmail.com>
2023-02-24 22:08:18 -0300

    Realizamos la conexion del servidor con la db

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { PORT } from "./config.js";
+
+import indexRoutes from "./routes/index.routes.js";
+
+const app = express();
+
+app.listen(PORT);
+
+app.use(indexRoutes);
+
+console.log(`server is runnig on port ${PORT} `);
