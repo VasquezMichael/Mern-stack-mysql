9b2cb61b0b266ca4265e999c48612f27175eeea2
VasquezMichael <vasquez.michael.steven@gmail.com>
2023-03-02 22:43:41 -0300

    Armamos las operaciones CRUD

--- server/controllers/task.controllers.js
+++ server/controllers/task.controllers.js
@@ -1,19 +1,51 @@
-export const getTask = (req, res) => {
-  res.send("obteniendo una tarea");
+import { pool } from "../db.js";
+
+export const getTasks = async (req, res) => {
+  const [result] = await pool.query(
+    "SELECT * FROM tasks ORDER BY createAt ASC"
+  );
+  console.log(result);
+  res.json(result);
 };
 
-export const getTasks = (req, res) => {
-  res.send("obteniendo tareas");
+export const getTask = async (req, res) => {
+  const [result] = await pool.query("SELECT * FROM tasks WHERE id = ?", [
+    req.params.id,
+  ]);
+  if (result.length === 0)
+    return res.status(404).json({ message: "task not found" });
+  res.json(result[0]);
 };
 
-export const createTask = (req, res) => {
-  res.send("creando tarea");
+export const createTask = async (req, res) => {
+  const { title, description } = req.body;
+  const [result] = await pool.query(
+    "INSERT INTO tasks(title, description) VALUES (?, ?) ",
+    [title, description]
+  );
+  console.log(result);
+  res.json({
+    id: result.insertId,
+    title,
+    description,
+  });
 };
 
-export const updateTask = (req, res) => {
-  res.send("actualizando tarea");
+export const updateTask = async (req, res) => {
+  const result = await pool.query("UPDATE tasks SET ? WHERE id = ?", [
+    req.body,
+    req.params.id,
+  ]);
+  res.json(result);
 };
 
-export const deleteTask = (req, res) => {
-  res.send("elimando tarea");
+export const deleteTask = async (req, res) => {
+  const [result] = await pool.query("DELETE FROM tasks WHERE id = ?", [
+    req.params.id,
+  ]);
+
+  if (result.affectedRows === 0)
+    return res.status(404).json({ message: "task not found" });
+
+  return res.sendStatus(204);
 };
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import { PORT } from "./config.js";
 import indexRoutes from "./routes/index.routes.js";
 import taskRoutes from "./routes/task.routes.js";
 const app = express();
+app.use(express.json());
 
 app.listen(PORT);
 app.use(taskRoutes);
